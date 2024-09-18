ebb4fbbda175d2d64ac83f6c144369ae8cc9b366
Tiago Farinas <tiagofarinas@gmail.com>
2024-09-18 15:27:03 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,6 @@
+//Task 1 - Create an Inventory Array of Product Object
+const inventory = [{name: Espresso , price:4 , quantity:10},
+    {name:Latte , price: 6.50, quantity:5},
+    {name:Cappuccino , price: 6, quantity:6},
+    {name: Mocha, price:5 , quantity:4},
+] // Initialization of array of objects
