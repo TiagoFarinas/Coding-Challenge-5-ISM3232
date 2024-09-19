26c5f79a53ed29842c721791452521d0a0ba8225
Tiago Farinas <tiagofarinas@gmail.com>
2024-09-19 11:22:53 -0400

    Initialize orders array

--- order_management_system.js
+++ order_management_system.js
@@ -1,6 +1,11 @@
 //Task 1 - Create an Inventory Array of Product Object
 const inventory = [{name: Espresso , price:4 , quantity:10},
-    {name:Latte , price: 6.50, quantity:5},
-    {name:Cappuccino , price: 6, quantity:6},
-    {name: Mocha, price:5 , quantity:4},
-] // Initialization of array of objects
+                   {name:Latte , price: 6.50, quantity:5},
+                   {name:Cappuccino , price: 6, quantity:6},
+                   {name: Mocha, price:5 , quantity:4}]
+ // Initialization of array of objects
+
+ //Task 2 - Create an Orders Array of Order Objects
+ const orders =[]
+ //I was a little consufed if this task was really supossed to be this simple
+ 
